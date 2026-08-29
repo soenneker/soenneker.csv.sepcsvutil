aba099301f98ee194b2d9374077971524086b233
soenneker <jake@soenneker.com>
2026-08-29 14:16:58 -0500

    Improve public API XML documentation

--- src/Soenneker.Csv.SepCsvUtil/Registrars/SepCsvUtilRegistrar.cs
+++ src/Soenneker.Csv.SepCsvUtil/Registrars/SepCsvUtilRegistrar.cs
@@ -12,6 +12,8 @@ public static class SepCsvUtilRegistrar
     /// <summary>
     /// Adds <see cref="ISepCsvUtil"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddSepCsvUtilAsSingleton(this IServiceCollection services)
     {
         services.TryAddSingleton<ISepCsvUtil, SepCsvUtil>();
@@ -22,6 +24,8 @@ public static class SepCsvUtilRegistrar
     /// <summary>
     /// Adds <see cref="ISepCsvUtil"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddSepCsvUtilAsScoped(this IServiceCollection services)
     {
         services.TryAddScoped<ISepCsvUtil, SepCsvUtil>();
